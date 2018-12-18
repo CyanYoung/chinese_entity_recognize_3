81fd37a93f49fe02f587a8a226a74eb4e1d3225e
yangjc <bupt_yjc@163.com>
2018-12-18 21:29:45 +0800

    add func

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,96 @@
+import os
+
 import json
 import pandas as pd
 
 import re
 
-from random import shuffle
+from random import shuffle, choice
+
+from util import load_pair, load_poly
+
+
+path_zh_en = 'dict/zh_en.csv'
+path_homo = 'dict/homo.csv'
+path_syno = 'dict/syno.csv'
+zh_en = load_pair(path_zh_en)
+homo_dict = load_poly(path_homo)
+syno_dict = load_poly(path_syno)
 
 
 def save(path, sents):
     with open(path, 'w') as f:
         json.dump(sents, f, ensure_ascii=False, indent=4)
+
+
+def dict2list(sents):
+    word_mat, label_mat = list(), list()
+    for pairs in sents.values():
+        words, labels = list(), list()
+        for pair in pairs:
+            words.append(pair['word'])
+            labels.append(pair['label'])
+        word_mat.append(words)
+        label_mat.append(labels)
+    return word_mat, label_mat
+
+
+def list2dict(word_mat, label_mat):
+    sents = dict()
+    for words, labels in zip(word_mat, label_mat):
+        text = ''.join(words)
+        pairs = list()
+        for word, label in zip(words, labels):
+            pair = dict()
+            pair['word'] = word
+            pair['label'] = label
+            pairs.append(pair)
+        sents[text] = pairs
+    return sents
+
+
+def select(part):
+    if part[0] == '[' and part[-1] == ']':
+        word = part[1:-1]
+        cands = set()
+        cands.add(word)
+        if word in syno_dict:
+            cands.update(syno_dict[word])
+        if word in homo_dict:
+            cands.update(homo_dict[word])
+        return choice(list(cands))
+    elif part[0] == '(' and part[-1] == ')':
+        word = part[1:-1]
+        return choice([word, ''])
+    else:
+        return part
+
+
+def generate(temps, slots, num):
+    word_mat, label_mat = list(), list()
+    for i in range(num):
+        parts = choice(temps)
+        words, labels = list(), list()
+        for part in parts:
+            if part in slots:
+                entity = choice(slots[part])
+                words.extend(entity)
+                labels.append('B-' + part)
+                if len(entity) > 1:
+                    labels.extend(['I-' + part] * (len(entity) - 1))
+            else:
+                word = select(part)
+                words.extend(word)
+                labels.extend(['O'] * len(word))
+        word_mat.append(words)
+        label_mat.append(labels)
+    return word_mat, label_mat
+
+
+def sync_shuffle(list1, list2):
+    pairs = list(zip(list1, list2))
+    shuffle(pairs)
+    return zip(*pairs)
 
 
 def label_sent(path):
@@ -41,45 +123,41 @@ def label_sent(path):
     return sents
 
 
-def dict2list(sents):
-    word_mat, label_mat = list(), list()
-    for pairs in sents.values():
-        words, labels = list(), list()
-        for pair in pairs:
-            words.append(pair['word'])
-            labels.append(pair['label'])
-        word_mat.append(words)
-        label_mat.append(labels)
-    return word_mat, label_mat
+def expand(word_mat, label_mat, extra_sents):
+    word_mat, label_mat = sync_shuffle(word_mat, label_mat)
+    bound = int(len(word_mat) * 0.9)
+    train_sents = list2dict(word_mat[:bound], label_mat[:bound])
+    train_sents.update(extra_sents)
+    test_sents = list2dict(word_mat[bound:], label_mat[bound:])
+    return train_sents, test_sents
 
 
-def list2dict(word_mat, label_mat):
-    sents = dict()
-    for words, labels in zip(word_mat, label_mat):
-        text = ''.join(words)
-        pairs = list()
-        for word, label in zip(words, labels):
-            pair = dict()
-            pair['word'] = word
-            pair['label'] = label
-            pairs.append(pair)
-        sents[text] = pairs
-    return sents
-
-
-def prepare(path_univ, path_train, path_test):
-    sents = label_sent(path_univ)
-    word_mat, label_mat = dict2list(sents)
-    pairs = list(zip(word_mat, label_mat))
-    shuffle(pairs)
-    word_mat, label_mat = zip(*pairs)
-    bound = int(len(sents) * 0.9)
-    save(path_train, list2dict(word_mat[:bound], label_mat[:bound]))
-    save(path_test, list2dict(word_mat[bound:], label_mat[bound:]))
+def prepare(paths):
+    temps = list()
+    with open(paths['template'], 'r') as f:
+        for line in f:
+            parts = line.strip().split()
+            temps.append(parts)
+    slots = dict()
+    files = os.listdir(paths['slot_dir'])
+    for file in files:
+        label = zh_en[os.path.splitext(file)[0]]
+        slots[label] = list()
+        with open(os.path.join(paths['slot_dir'], file), 'r') as f:
+            for line in f:
+                slots[label].append(line.strip())
+    word_mat, label_mat = generate(temps, slots, num=1000)
+    extra_sents = label_sent(paths['extra'])
+    train_sents, test_sents = expand(word_mat, label_mat, extra_sents)
+    save(paths['train'], train_sents)
+    save(paths['test'], test_sents)
 
 
 if __name__ == '__main__':
-    path_univ = 'data/univ.csv'
-    path_train = 'data/train.json'
-    path_test = 'data/test.json'
-    prepare(path_univ, path_train, path_test)
+    paths = dict()
+    paths['train'] = 'data/train.json'
+    paths['test'] = 'data/test.json'
+    paths['template'] = 'data/template.txt'
+    paths['slot_dir'] = 'data/slot'
+    paths['extra'] = 'data/extra.csv'
+    prepare(paths)
